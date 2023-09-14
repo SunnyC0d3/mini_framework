3dc3f3ea36eac0a60c99095428dfaf2e8e8d1e44
SunnyC0d3 <sunny.singh@lnpg.co.uk>
2023-09-14 15:41:36 +0000

    Removed comments

--- routes.php
+++ routes.php
@@ -3,6 +3,3 @@
 use Demo\Controllers\TestController;
 
 $router->get( '/testing', [ new TestController(), 'index' ] )->middleware( [ 'guest' ] );
-// $router->delete( '/', [ new TestController(), 'delete' ] );
-// $router->patch( '/', [ new TestController(), 'patch' ] );
-// $router->put( '/', [ new TestController(), 'put' ] );
